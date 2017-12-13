0edd5b94234066a10562efa3773bf650181a5baa
zhangype <zhangype@yonyou.com>
2017-12-13 14:33:47 +0800

    集成logback日志

--- src/main/java/com/zhangype/myboot/modules/sys/LoginController.java
+++ src/main/java/com/zhangype/myboot/modules/sys/LoginController.java
@@ -1,6 +1,8 @@
 package com.zhangype.myboot.modules.sys;
 
 import com.zhangype.myboot.config.properties.TestProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private TestProperties properties;
 
     @RequestMapping("/")
     public String home() {
+        LOGGER.info("value={}", properties.getValue());
         return "Hello World!" + properties.getValue();
     }
 }
